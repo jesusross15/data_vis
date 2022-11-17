81bfc501e54d4b97570e3aafc62431f504683b98
Jesus Ross <91804598+jesusross15@users.noreply.github.com>
2022-11-16 23:18:57 -0500

    scatter plot

--- scatter_squares.py
+++ scatter_squares.py
@@ -9,12 +9,15 @@ y_values = [x**2 for x in x_values]
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=10)
+# using a ccolor map
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
+# This is to get rid of the default scientific notation in the x and y axis
+ax.ticklabel_format(useOffset=False, style='plain')
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
-ax.set_title("Value", fontsize=14)
-ax.set_title("Square of Value", fontsize=14)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
 
 # Set size of tick labels
 ax.tick_params(axis='both', which='major', labelsize=14)
