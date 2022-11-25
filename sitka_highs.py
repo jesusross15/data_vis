1cefa8c56c8c341d2b5f9f7e766f4246144cc6a6
Jesus Ross <91804598+jesusross15@users.noreply.github.com>
2022-11-24 23:56:55 -0500

    adding datetime module

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,6 +1,6 @@
 import csv
-
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 filename = "data_vis/sitka.csv"
 with open(filename) as f:
@@ -10,22 +10,23 @@ with open(filename) as f:
     """for index, column_header in enumerate(header_row): 
         print(index, column_header)"""
     
-    # Get high temperatures from this file.
-    highs = []
+    # Get dates and high temperatures from this file.
+    dates, highs = [], []
     for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[5]) 
+        dates.append(current_date)
         highs.append(high)
-
-print(highs)
 
 # Plot the high temperatures.
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(highs, c='red')
+ax.plot(dates, highs, c='red')
 
 # Format plot.
 plt.title("Daily high temperatures, July 2018", fontsize=24) 
 plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
 
