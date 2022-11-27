5975f2f2b56614823380a634e40b9c82d33303b3
Jesus Ross <91804598+jesusross15@users.noreply.github.com>
2022-11-26 23:38:36 -0500

    plotting the highs and lows tempterature for the year of 2018

--- sitka_highs.py
+++ sitka_highs.py
@@ -2,7 +2,7 @@ import csv
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-filename = "data_vis/sitka.csv"
+filename = "data_vis/sitka_2018_simple.csv"
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -10,21 +10,24 @@ with open(filename) as f:
     """for index, column_header in enumerate(header_row): 
         print(index, column_header)"""
     
-    # Get dates and high temperatures from this file.
-    dates, highs = [], []
+    # Get dates, and high and low temperatures from this file
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[5]) 
+        low = int(row[6])
         dates.append(current_date)
         highs.append(high)
+        lows.append(low)
 
-# Plot the high temperatures.
+# Plot the low and high temperatures.
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
 ax.plot(dates, highs, c='red')
+ax.plot(dates, lows, c='blue')
 
 # Format plot.
-plt.title("Daily high temperatures, July 2018", fontsize=24) 
+plt.title("Daily high and low temperatures - 2018", fontsize=24) 
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
