bf62f6f02797b373b483687cdbdd6d25d5a2f9dd
Jesus Ross <91804598+jesusross15@users.noreply.github.com>
2022-11-27 22:32:22 -0500

    extracting magnitudes

---
+++ eq_explore_data.py
@@ -0,0 +1,17 @@
+import json
+
+# Explore the structure of the data
+filename = "data_vis/downloadedData/eq_data_1_day_m1.json"
+with open(filename) as f:
+    all_eq_data = json.load(f)
+
+all_eq_dicts = all_eq_data['features']
+
+mags = []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag'] 
+    mags.append(mag)
+
+print(mags[:10])
+    
+
